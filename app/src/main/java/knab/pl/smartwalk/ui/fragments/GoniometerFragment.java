734c95d1b583edeb0ff3691e0692a6926e17c764
Aisolug <aisolug@gmail.com>
2016-10-14 11:02:44 +0200

    Goniometry changes

--- app/src/main/java/knab/pl/smartwalk/ui/fragments/GoniometerFragment.java
+++ app/src/main/java/knab/pl/smartwalk/ui/fragments/GoniometerFragment.java
@@ -1,12 +1,17 @@
 package knab.pl.smartwalk.ui.fragments;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import javax.inject.Inject;
 
@@ -33,7 +38,25 @@ public class GoniometerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_goniometer, container, false);
+
+        View rootView =  inflater.inflate(R.layout.fragment_goniometer, container, false);
+        RelativeLayout relativeLayout = (RelativeLayout) rootView.findViewById(R.id.rect);
+        relativeLayout.addView(new Rectangle(getActivity()));
+
+        return rootView;
+    }
+    private class Rectangle extends View {
+        Paint paint = new Paint();
+
+        public Rectangle(Context context) {
+            super(context);
+        }
+        @Override
+        public void onDraw(Canvas canvas) {
+            paint.setColor(Color.GREEN);
+            Rect rect = new Rect(20, 6, 100, 52);
+            canvas.drawRect(rect, paint );
+        }
     }
 
     @Override
